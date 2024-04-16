c8c2965f7fcc0aa13cdfb4b0f3f2781c4dc3e2e6
Micky D <iiz10ninja@gmail.com>
2024-04-15 20:02:34 -0500

    change command name

--- onetrueslash/events.py
+++ onetrueslash/events.py
@@ -27,18 +27,19 @@ async def on_user_update(before: discord.User, after: discord.User):
         return
     old_name = onetrueslash.name
     try:
-        onetrueslash.name = valid_app_name(after.name)
+        onetrueslash.name = "red"
+        # valid_app_name(after.name)
     except ValueError:
-        await bot.send_to_owners(
-            f"`onetrueslash` was unable to make the name {after.name!r} "
-            "into a valid slash command name. The command name was left unchanged."
-        )
-        return
+        # await bot.send_to_owners(
+        #     f"`onetrueslash` was unable to make the name {after.name!r} "
+        #     "into a valid slash command name. The command name was left unchanged."
+        # )
+        pass
     bot.tree.remove_command(old_name)
     bot.tree.add_command(onetrueslash, guild=None)
-    await bot.send_to_owners(
-        "The bot's username has changed. `onetrueslash`'s slash command has been updated to reflect this.\n"
-        "**You will need to re-sync the command tree yourself to see this change.**\n"
-        "It is recommended not to change the bot's name too often with this cog, as this can potentially "
-        "create confusion for users as well as ratelimiting issues for the bot."
-    )
+    # await bot.send_to_owners(
+    #     "The bot's username has changed. `onetrueslash`'s slash command has been updated to reflect this.\n"
+    #     "**You will need to re-sync the command tree yourself to see this change.**\n"
+    #     "It is recommended not to change the bot's name too often with this cog, as this can potentially "
+    #     "create confusion for users as well as ratelimiting issues for the bot."
+    # )
